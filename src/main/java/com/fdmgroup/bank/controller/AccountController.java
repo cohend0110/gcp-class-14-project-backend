6e216388067b8700965b7e3eb852c9643b62bcf0
cohend0110 <dcohen0110@gmail.com>
2026-02-23 03:41:39 -0500

    added "trigger" comment

--- src/main/java/com/fdmgroup/bank/controller/AccountController.java
+++ src/main/java/com/fdmgroup/bank/controller/AccountController.java
@@ -27,6 +27,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+//trigger
 @RestController
 @RequestMapping("/api/v1/accounts")
 @CrossOrigin(origins = "https://my-react-app-135897197036.us-central1.run.app", allowedHeaders = "*")
